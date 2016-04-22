5fb1ee8dddef70acb1b5d1aaf38060b99a215123
Mauricio Matamala <mauriciomatamala@hotmail.com>
2016-04-22 12:37:31 +0200

    Controlador terminado

---
+++ controlador/Controlador.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require "../vendor/autoload.php";
+
+
+$saludoRecibido = $_GET['saludo'];
+
+use sumaysigue\modelo\Saludo;
+$objetoSaludo = new Saludo();
+$objetoSaludo->setSaludo($saludoRecibido);
+
+$saludo = $objetoSaludo->formatSaludo();
+
+require "../vista/plantillaSaludo.php";
+
---
+++ modelo/Saludo.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Saludo
+ *
+ * @author mauri
+ */
+namespace sumaysigue\modelo;
+
+class Saludo {
+    private $saludo = "";
+    
+    public function setSaludo($saludo){
+       
+    }
+    
+    public function formatSaludo(){
+        return "";
+    }
+}
