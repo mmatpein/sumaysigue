9b2909b4e0f3605929bf5186775a08db934e3117
Karel Rubí <karelrubi@gmail.com>
2016-04-22 12:19:56 +0200

    Plantilla terminada

---
+++ vista/plantillaSaludo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Plantilla saludo</title>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<h1>Este es el saludo</h1>
+
+		<p style="color:red">
+			<?php echo $saludo;?>
+		</p>
+	</body>
+</html>
